5f23a44410be5c1e69a13278fe1a871587d810ff
Zaryaab <zariab64@gmail.com>
2023-08-09 14:39:39 +0500

    React Boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+import "./App.css";
+
+function App() {
+  return (
+    <section className="min-h-screen bg-thm text-white flex flex-col">
+      Hello World!
+    </section>
+  );
+}
+
+export default App;
